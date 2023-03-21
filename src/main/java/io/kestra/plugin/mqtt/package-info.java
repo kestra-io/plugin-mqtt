35ba1977cfcaa7f5867242d6eb71a83ae28cfecf
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:34:24 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/mqtt/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using a MQTT message broker.",
+    categories = PluginSubGroup.PluginCategory.MESSAGING
+)
+package io.kestra.plugin.mqtt;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
