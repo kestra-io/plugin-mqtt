035002195ef50875ff85cf2614e6dab3fa1c3b9e
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-09-05 15:52:08 +0200

    refactor(test): adjust to last oss queue signature
    relate to kestra-io/kestra#1981

--- src/test/java/io/kestra/plugin/mqtt/TriggerTest.java
+++ src/test/java/io/kestra/plugin/mqtt/TriggerTest.java
@@ -68,10 +68,10 @@ class TriggerTest {
 
             // wait for execution
             executionQueue.receive(TriggerTest.class, execution -> {
-                last.set(execution);
+                last.set(execution.getLeft());
 
                 queueCount.countDown();
-                assertThat(execution.getFlowId(), is("trigger"));
+                assertThat(execution.getLeft().getFlowId(), is("trigger"));
             });
 
             Publish task = Publish.builder()
