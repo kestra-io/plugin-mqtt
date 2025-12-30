d2e3815454b99823c825acbddb9fe9813a7f4662
ben8t <pimpaudben@gmail.com>
2025-12-30 15:40:11 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/mqtt/package-info.java
+++ src/main/java/io/kestra/plugin/mqtt/package-info.java
@@ -1,6 +1,10 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using a MQTT message broker.",
-    categories = PluginSubGroup.PluginCategory.MESSAGING
+    categories = PluginSubGroup.PluginCategory.MESSAGING,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.mqtt;
 
