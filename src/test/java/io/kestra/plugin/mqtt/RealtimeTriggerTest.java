681ce5e7a5a410cc954db8d1ed25efdb8252b5e8
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-12 02:24:36 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/mqtt/RealtimeTriggerTest.java
+++ src/test/java/io/kestra/plugin/mqtt/RealtimeTriggerTest.java
@@ -2,118 +2,78 @@ package io.kestra.plugin.mqtt;
 
 import java.util.Map;
 import java.util.Objects;
-import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Test;
-
-import com.google.common.collect.ImmutableMap;
 
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.queues.DispatchQueueInterface;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.runners.FlowListeners;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.IdUtils;
-import io.kestra.core.utils.TestsUtils;
-import io.kestra.jdbc.runner.JdbcScheduler;
 import io.kestra.plugin.mqtt.services.SerdeType;
-import io.kestra.scheduler.AbstractScheduler;
-import io.kestra.worker.DefaultWorker;
 
-import io.micronaut.context.ApplicationContext;
 import jakarta.inject.Inject;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@KestraTest
+@KestraTest(startRunner = true, startScheduler = true)
 class RealtimeTriggerTest {
     @Inject
-    private ApplicationContext applicationContext;
-
-    @Inject
-    private FlowListeners flowListenersService;
+    private RunContextFactory runContextFactory;
 
     @Inject
     private DispatchQueueInterface<Execution> executionQueue;
 
     @Inject
     protected LocalFlowRepositoryLoader repositoryLoader;
-    @Inject
-    private RunContextFactory runContextFactory;
 
     @SuppressWarnings("unchecked")
     @Test
     void flow() throws Exception {
-        // mock flow listeners
-        CountDownLatch queueCount = new CountDownLatch(1);
-        AtomicReference<Execution> last = new AtomicReference<>();
+        var queueCount = new CountDownLatch(1);
+        var last = new AtomicReference<Execution>();
 
-        // scheduler
-        try (DefaultWorker worker = applicationContext.createBean(DefaultWorker.class, UUID.randomUUID().toString(), 8, null)) {
-            try (
-                AbstractScheduler scheduler = new JdbcScheduler(
-                    this.applicationContext,
-                    this.flowListenersService
-                );
-            ) {
-                // wait for execution
-                executionQueue.addListener(execution -> {
-                    last.set(execution);
-                    queueCount.countDown();
-                    assertThat(execution.getFlowId(), is("realtime"));
-                });
+        executionQueue.addListener(execution -> {
+            last.set(execution);
+            queueCount.countDown();
+            assertThat(execution.getFlowId(), is("realtime"));
+        });
 
-                String messageText = "hello trigger";
-                String triggerText = "Trigger is completed";
+        repositoryLoader.load(
+            Objects.requireNonNull(
+                RealtimeTriggerTest.class.getClassLoader().getResource("flows/realtime.yaml")
+            )
+        );
 
-                Publish task = Publish.builder()
-                    .id(RealtimeTriggerTest.class.getSimpleName())
-                    .type(Publish.class.getName())
-                    .server(Property.ofValue("tcp://localhost:1883"))
-                    .clientId(Property.ofValue(IdUtils.create()))
-                    .topic(Property.ofValue("test/realtime/trigger"))
-                    .serdeType(Property.ofValue(SerdeType.JSON))
-                    .retain(Property.ofValue(true))
-                    .mqttVersion(Property.ofValue(AbstractMqttConnection.Version.V5))
-                    .from(
-                        Map.of(
-                            "message", messageText,
-                            "notification", triggerText
-                        )
-                    )
-                    .build();
+        var task = Publish.builder()
+            .id(RealtimeTriggerTest.class.getSimpleName())
+            .type(Publish.class.getName())
+            .server(Property.ofValue("tcp://localhost:1883"))
+            .clientId(Property.ofValue(IdUtils.create()))
+            .topic(Property.ofValue("test/realtime/trigger"))
+            .serdeType(Property.ofValue(SerdeType.JSON))
+            .retain(Property.ofValue(true))
+            .mqttVersion(Property.ofValue(AbstractMqttConnection.Version.V5))
+            .from(
+                Map.of(
+                    "message", "hello trigger",
+                    "notification", "Trigger is completed"
+                )
+            )
+            .build();
 
-                worker.run();
-                scheduler.run();
+        task.run(runContextFactory.of(Map.of()));
+        assertThat(queueCount.await(1, TimeUnit.MINUTES), is(true));
 
-                repositoryLoader.load(
-                    Objects.requireNonNull(
-                        RealtimeTriggerTest.class.getClassLoader()
-                            .getResource("flows/realtime.yaml")
-                    )
-                );
-
-                task.run(TestsUtils.mockRunContext(runContextFactory, task, ImmutableMap.of()));
-
-                boolean await = queueCount.await(1, TimeUnit.MINUTES);
-                assertThat(await, is(true));
-
-                Map<String, String> payload = (Map<String, String>) last.get()
-                    .getTrigger()
-                    .getVariables()
-                    .get("payload");
-
-                assertThat(payload.size(), is(2));
-
-                assertThat(payload.get("message"), is(messageText));
-                assertThat(payload.get("notification"), is(triggerText));
-            }
-        }
+        var payload = (Map<String, String>) last.get().getTrigger().getVariables().get("payload");
+        assertThat(payload.size(), is(2));
+        assertThat(payload.get("message"), is("hello trigger"));
+        assertThat(payload.get("notification"), is("Trigger is completed"));
     }
 }
